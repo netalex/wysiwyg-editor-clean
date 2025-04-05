4f5ff94dcdaeff412e410492bef533512d89d180
Alessandro Aprile <alessandro.aprile@apprendo.tech>
2025-04-05 17:34:52 +0000

    AA progetto minimale

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 4321
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true
+      },
+      hmr: {
+        clientPort: 4321
+      }
+    }
+  }
+});
